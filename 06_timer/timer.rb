3f512cf0563f03ebc07804cd5cdd3c2c81dcf57c
alexanderspace100 <alexanderspace100@gmail.com>
2017-03-22 09:35:01 +0200

    Done

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,12 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def seconds
+    @seconds = 0
+  end
+
+  def time_string
+    @time_string = Time.at(@seconds).utc.to_s.split[1]
+  end
 end
